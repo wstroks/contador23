f475e30a132cf27419d3ab9e47529834344de1f1
Washington Pagotto Batista <wstroks@hotmail.com>
2019-12-30 19:36:34 -0300

    Initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="footer footer-black  footer-white ">
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="footer-nav">
+                <ul>
+                    <!--
+                    <li>
+                        <a href="https://www.creative-tim.com" target="_blank">{{ __('Creative Tim') }}</a>
+                    </li>
+                    <li>
+                        <a href="https://updivision.com" target="_blank">{{ __('UpDivision') }}</a>
+                    </li>
+                    <li>
+                        <a href="http://blog.creative-tim.com/" target="_blank">{{ __('Blog') }}</a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/license" target="_blank">{{ __('Licenses') }}</a>
+                    </li> !-->
+                </ul>
+            </nav>
+            <div class="credits ml-auto">
+                <span class="copyright">
+                    ©
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>{{ __(', criado') }}{{ __(' por ') }}<a class="@if(Auth::guest()) text-white @endif"  target="_blank">{{ __('Lucas') }}</a>{{ __(' e ') }}<a class="@if(Auth::guest()) text-white @endif" target="_blank" >{{ __('Washington') }}</a>
+                </span>
+            </div>
+        </div>
+    </div>
+</footer>
